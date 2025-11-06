66d79f70e20383361cf7e2eac58817f2bdcd8dc9
d1r1 <me@d1r1.me>
2025-11-06 20:51:57 +0400

    feat(indexer): set concurrency to 1 for contract code

--- apps/indexer/lib/indexer/fetcher/contract_code.ex
+++ apps/indexer/lib/indexer/fetcher/contract_code.ex
@@ -50,7 +50,7 @@ defmodule Indexer.Fetcher.ContractCode do
   @behaviour BufferedTask
 
   @max_batch_size 10
-  @max_concurrency 4
+  @max_concurrency 1
   @defaults [
     flush_interval: :timer.seconds(3),
     max_concurrency: @max_concurrency,
